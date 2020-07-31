ece8f88c2789f7064133dd9634bb6f64fe6c9a13
Илья Мельников <ilyamelnikov123@yandex.ru>
2020-07-31 22:09:46 +0300

    Создал хелп ворлд

---
+++ test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            if (int.TryParse("1", out int result))
+            {
+                Console.WriteLine(result);
+            }
+            
+        }
+    }
+}
